1937280bd95a60ddd616fa6d0b24215b664469c6
[Phongtran1998] <hoang_phong98@me.com>
2018-03-03 19:46:43 -0500

    Third commit

---
+++ code/src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+
+const Home = () => {
+    return(
+        <View>
+            <TouchableOpacity>
+                <Text>Press</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default Home;
